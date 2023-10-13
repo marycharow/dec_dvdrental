c53912694bd196d40cbf51bdb4accf5670914462
trevorrobertsjr <trevorrobertsjr>
2023-10-13 02:39:33 +0000

    begin silver dim_store and fact_rentals notebooks
    Complete:
    Source tables for dim_store have been imported and tested.
    Remaining
    1. Perform joins to create dim_store
    2. Create DF's for fact_rentals source tables
    3. Test fact_rentals DF's
    4. Perform joins to form fact_rentals table

---
+++ notebooks/silver_fact_rentals.py
@@ -0,0 +1,102 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Silver Zone: Fact Table: fact_rentals
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Import prerequisite libraries
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col, json_tuple, current_timestamp, concat_ws, md5
+from delta.tables import DeltaTable
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Read in raw Airbyte tables to dataframes
+
+# COMMAND ----------
+
+df_store = spark.read.table("main.default._airbyte_raw_store")
+df_address = spark.read.table("main.default._airbyte_raw_address")
+df_city = spark.read.table("main.default._airbyte_raw_city")
+df_country = spark.read.table("main.default._airbyte_raw_country")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Normalize dataframes
+
+# COMMAND ----------
+
+df_store_norm = df_store.select(col("_airbyte_ab_id"),json_tuple(col("_airbyte_data"),"store_id","manager_staff_id","address_id","last_update")) \
+    .toDF("_airbyte_ab_id","store_id","manager_staff_id","address_id","last_update")
+df_address_norm = df_address.select(col("_airbyte_ab_id"),json_tuple(col("_airbyte_data"),"address_id","address","address2","district","city_id","postal_code","phone","last_update")) \
+    .toDF("_airbyte_ab_id","address_id","address","address2","district","city_id","postal_code","phone","last_update")
+df_city_norm = df_city.select(col("_airbyte_ab_id"),json_tuple(col("_airbyte_data"),"city_id","city","last_update")) \
+    .toDF("_airbyte_ab_id","city_id","city","last_update")
+df_country_norm = df_country.select(col("_airbyte_ab_id"),json_tuple(col("_airbyte_data"),"country_id","country","last_update")) \
+    .toDF("_airbyte_ab_id","country_id","country","last_update")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Run Tests with Great Expectations
+
+# COMMAND ----------
+
+from great_expectations.dataset import SparkDFDataset
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Test store table
+
+# COMMAND ----------
+
+ge_df_store = SparkDFDataset(df_store_norm)
+expectation_store = ge_df_store.expect_column_values_to_not_be_null("store_id")
+if not expectation_store["success"]: 
+    raise Exception(expectation_store)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Test address table
+
+# COMMAND ----------
+
+ge_df_address = SparkDFDataset(df_address_norm)
+expectation_address = ge_df_address.expect_column_values_to_not_be_null("address_id")
+if not expectation_address["success"]: 
+    raise Exception(expectation_address)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Test city table
+
+# COMMAND ----------
+
+ge_df_city = SparkDFDataset(df_city_norm)
+expectation_city = ge_df_city.expect_column_values_to_not_be_null("city_id")
+if not expectation_city["success"]: 
+    raise Exception(expectation_city)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Test country table
+
+# COMMAND ----------
+
+ge_df_country = SparkDFDataset(df_country_norm)
+expectation_country = ge_df_country.expect_column_values_to_not_be_null("country_id")
+if not expectation_country["success"]: 
+    raise Exception(expectation_country)
+
+# COMMAND ----------
+
+
